85d7f9e3172f15de9103caf0da6728df26bdaa2f
Sai <Sai@LAPTOP-SCE9GJLM>
2020-06-24 11:17:17 +0530

    first commit

---
+++ milestone1/src/LanguageBasics/first.java
@@ -0,0 +1,7 @@
+package LanguageBasics;
+
+public class first {
+	public static void main(String[] args) {
+		   System.out.println(args[0]+" Technologies "+args[1]);	  }
+
+}
---
+++ milestone1/src/LanguageBasics/second.java
@@ -0,0 +1,7 @@
+package LanguageBasics;
+
+public class second {
+	public static void main(String[] args){
+		   System.out.println("Welcome "+args[0]);	  }
+
+}
